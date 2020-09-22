65f9caade3ae39601bc9d7b0b687b37230b173eb
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-09-22 14:15:14 -0300

    Create user service is complete Authenticate user service is complete

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,22 +1,60 @@
+import AppError from '@shared/errors/AppError'
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
+import IHashProvider from '../providers/HashProvider/models/IHashProvider'
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
 import IUserRepository from '../repositories/IUserRepository'
 import AuthenticateUserService from './AuthenticateUserService'
 
 let authenticateUser: AuthenticateUserService
-let usersRepository: IUserRepository
+let fakeHashProvider: IHashProvider
+let fakeUsersRepository: IUserRepository
 
 describe('AuthenticateUser', () => {
   beforeEach(() => {
-    usersRepository = new FakeUsersRepository()
-    authenticateUser = new AuthenticateUserService(usersRepository)
+    fakeUsersRepository = new FakeUsersRepository()
+    fakeHashProvider = new FakeHashProvider()
+    authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    )
   })
+
   it('should be able to authenticate a user', async () => {
-    await usersRepository.create({
+    await fakeUsersRepository.create({
       email: 'johndoe@example.com',
       password: '12345678'
     })
-    const response = await authenticateUser.execute({ email: '', password: '' })
+    const response = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '12345678'
+    })
 
     expect(response).toHaveProperty('token')
+  })
+
+  it('should not be able to authenticate a user with wrong email', async () => {
+    await fakeUsersRepository.create({
+      email: 'johndoe@example.com',
+      password: '12345678'
+    })
+    await expect(
+      authenticateUser.execute({
+        email: 'wrong-email@email.com',
+        password: '12345678'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to authenticate a user with wrong password', async () => {
+    await fakeUsersRepository.create({
+      email: 'johndoe@example.com',
+      password: '12345678'
+    })
+    await expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-password'
+      })
+    ).rejects.toBeInstanceOf(AppError)
   })
 })
--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,6 +1,12 @@
 import { inject, injectable } from 'tsyringe'
+import { sign } from 'jsonwebtoken'
+
+import AppError from '@shared/errors/AppError'
 import User from '../infra/typeorm/entities/User'
+import IHashProvider from '../providers/HashProvider/models/IHashProvider'
 import IUserRepository from '../repositories/IUserRepository'
+
+import authConfig from '@config/auth'
 
 interface IRequest {
   email: string
@@ -16,10 +22,34 @@ interface IResponse {
 export default class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUserRepository
+    private usersRepository: IUserRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider
   ) {}
 
-  public async execute({ email, password }: IRequest) {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new AppError('Email or password incorrect.', 401)
+    }
+
+    const correctPassword = await this.hashProvider.compareHash(
+      password,
+      user.password
+    )
+
+    if (!correctPassword) {
+      throw new AppError('Email or password incorrect.', 401)
+    }
+
+    const { expiresIn, secret } = authConfig.jwt
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn
+    })
+
+    return { user, token }
   }
 }
