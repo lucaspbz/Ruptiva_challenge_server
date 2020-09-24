3f7f35d1ef73f29f17dcff47aa681d653e26f848
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-09-24 12:14:35 -0300

    route for listing users repositories is functional

--- src/modules/repos/infra/http/routes/repos.routes.ts
+++ src/modules/repos/infra/http/routes/repos.routes.ts
@@ -4,6 +4,7 @@ import { container } from 'tsyringe'
 import CreateReposService from '@modules/repos/services/CreateReposService'
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import validateFields from '@modules/repos/infra/http/middlewares/validateFields'
+import ListUsersReposService from '@modules/repos/services/ListUserReposService'
 
 const reposRouter = Router()
 
@@ -18,6 +19,16 @@ reposRouter.post('/', validateFields, async (request, response) => {
   const repo = await createRepo.execute({ user_id: id, description, full_name, owner_avatar_url, owner_login })
 
   return response.json(repo)
+})
+
+reposRouter.get('/', async (request, response) => {
+  const { id } = request.user
+
+  const listRepos = container.resolve(ListUsersReposService)
+
+  const repos = await listRepos.execute(id)
+
+  return response.json(repos)
 })
 
 
